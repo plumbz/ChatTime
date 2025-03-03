b61640efb5dd4425d9f693c542c90382f62748cd
plumbz <plumby.eschenbacher@yahoo.com>
2025-03-03 20:35:05 +0100

    added system message and corrected styling, downgraded giftedchat to 2.6.0 and added keyboard- controller

--- components/Chat.js
+++ components/Chat.js
@@ -1,34 +1,42 @@
 import { GiftedChat } from "react-native-gifted-chat";
 import { useState, useEffect } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Platform, KeyboardAvoidingView } from 'react-native';
 
 const Chat = ({ route, navigation }) => {
-    const { name, backgroundColor } = route.params;
+    const { name, backgroundColor } = route.params; //handle the props
     const [messages, setMessages] = useState([]);
     const onSend = (newMessages) => {
         setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages))
     }
+    // Render the messages
     useEffect(() => {
         setMessages([
             {
                 _id: 1,
-                text: "Hello developer",
+                text: 'Hello developer',
                 createdAt: new Date(),
                 user: {
                     _id: 2,
-                    name: "React Native",
-                    avatar: "https://placeimg.com/140/140/any",
+                    name: 'React Native',
+                    avatar: 'https://placeimg.com/140/140/any',
                 },
+            },
+            {
+                _id: 2,
+                text: 'This is a system message',
+                createdAt: new Date(),
+                system: true,
             },
         ]);
     }, []);
-
+    // update the Title with the entered name
     useEffect(() => {
         navigation.setOptions({ title: name });
     }, []);
 
     return (
         <View style={[styles.container, { backgroundColor: backgroundColor }]}>
+            {/* Feed GiftedChat with messages */}
             <GiftedChat
                 messages={messages}
                 onSend={messages => onSend(messages)}
@@ -36,6 +44,8 @@ const Chat = ({ route, navigation }) => {
                     _id: 1
                 }}
             />
+            {/* Adjust keyboard according to OS being used */}
+            {Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : <KeyboardAvoidingView behavior="padding" />}
         </View>
     );
 }
@@ -43,8 +53,6 @@ const Chat = ({ route, navigation }) => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
     }
 });
 
